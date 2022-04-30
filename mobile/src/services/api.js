0058e3db4e9362091897c86e75150fca11105adf
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-04-30 18:14:25 -0300

    update ip to test

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.101:3333'
+  baseURL: "http://172.17.0.1:3333",
 });
 
 export default api;
