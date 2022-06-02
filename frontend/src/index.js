e6d9be865224dc46b4fc526ec44679fb136ca59d
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-06-02 06:43:20 -0300

    lint: adjust lint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './css/global.css';
-import './css/tailwind-globaly.css';
-import 'react-toastify/dist/ReactToastify.css';
+import "./css/global.css";
+import "./css/tailwind-globaly.css";
+import "react-toastify/dist/ReactToastify.css";
 
-import App from './App';
+import App from "./App";
 
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<React.StrictMode>
-  <App />
-</React.StrictMode>);
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { FiPower, FiTrash2 } from 'react-icons/fi';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FiPower, FiTrash2 } from "react-icons/fi";
 
-import api from '../../services/api';
-
-import './styles.css';
-
-import logoImg from '../../assets/logo.svg';
+import api from "../../services/api";
+import Modal from "../../components/Modal";
+import "./styles.css";
+import logoImg from "../../assets/logo.svg";
 
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
 
   const navigate = useNavigate();
 
-  const ongId = localStorage.getItem('ongId');
-  const ongName = localStorage.getItem('ongName');
+  const ongId = localStorage.getItem("ongId");
+  const ongName = localStorage.getItem("ongName");
 
   useEffect(() => {
-    api.get('profile', {
-      headers: {
-        Authorization: ongId,
-      }
-    }).then(response => {
-      setIncidents(response.data);
-    })
+    api
+      .get("profile", {
+        headers: {
+          Authorization: ongId,
+        },
+      })
+      .then((response) => {
+        setIncidents(response.data);
+      });
   }, [ongId]);
 
   async function handleDeleteIncident(id) {
@@ -31,53 +32,69 @@ export default function Profile() {
       await api.delete(`incidents/${id}`, {
         headers: {
           Authorization: ongId,
-        }
+        },
       });
 
-      setIncidents(incidents.filter(incident => incident.id !== id));
+      setIncidents(incidents.filter((incident) => incident.id !== id));
     } catch (err) {
-      alert('Erro ao deleter caso. Tente novamente.');
+      alert("Erro ao deleter caso. Tente novamente.");
     }
   }
 
   function handleLogout() {
     localStorage.clear();
 
-    navigate('/');
+    navigate("/");
   }
 
   return (
-    <div className="profile-container">
-      <header>
-        <img src={logoImg} alt='Be The Hero' />
-        <span>Bem Vinda, {ongName}</span>
+    <>
+      <div className="profile-container">
+        <header>
+          <img src={logoImg} alt="Be The Hero" />
+          <span>Bem Vinda, {ongName}</span>
 
-        <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button className="flex justify-center items-center"onClick={handleLogout} type="button">
-          <FiPower size={18} color="#E02041" />
-        </button>
-      </header>
+          <Link className="button" to="/incidents/new">
+            Cadastrar novo caso
+          </Link>
+          <button
+            className="flex justify-center items-center"
+            onClick={handleLogout}
+            type="button"
+          >
+            <FiPower size={18} color="#E02041" />
+          </button>
+        </header>
 
-      <h1>Casos cadastrados:</h1>
+        <h1>Casos cadastrados:</h1>
 
-      <ul>
-        {incidents.map(incident => (
-          <li key={incident.id} >
-            <strong>CASO:</strong>
-            <p>{incident.title}</p>
+        <ul>
+          {incidents.map((incident) => (
+            <li key={incident.id}>
+              <strong>CASO:</strong>
+              <p>{incident.title}</p>
 
-            <strong>DESCRIÇÂO:</strong>
-            <p>{incident.description}</p>
+              <strong>DESCRIÇÂO:</strong>
+              <p>{incident.description}</p>
 
-            <strong>VALOR:</strong>
-            <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
+              <strong>VALOR:</strong>
+              <p>
+                {Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(incident.value)}
+              </p>
 
-            <button onClick={() => handleDeleteIncident(incident.id)} type="button">
-              <FiTrash2 size={20} color="#a8a8b3" />
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+              <button
+                onClick={() => handleDeleteIncident(incident.id)}
+                type="button"
+              >
+                <FiTrash2 size={20} color="#a8a8b3" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
