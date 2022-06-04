0e1c6d29ea171cc0d19eda24c734e38b4c7325f8
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-06-04 04:26:09 -0300

    feat: add img when no incidents

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -5,6 +5,7 @@ import { FiPower, FiTrash2 } from "react-icons/fi";
 import api from "../../services/api";
 import Modal from "../../components/Modal";
 import logoImg from "../../assets/logo.svg";
+import heroImg from "../../assets/hero.png";
 
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
@@ -90,46 +91,54 @@ export default function Profile() {
             </button>
           </div>
         </header>
-
-        <h1 className="mt-20 mb-6">Casos cadastrados:</h1>
-
+        {incidents.length > 0 && (
+          <h1 className="mt-20 mb-6">Casos cadastrados:</h1>
+        )}
         <ul className="grid grid-cols-3 gap-3 list-none">
-          {incidents.map((incident) => (
-            <li
-              className="p-6 rounded relative shadow flex justify-between"
-              key={incident.id}
-            >
-              <div>
-                <strong className="block mb-1 text-zinc-600">CASO:</strong>
-                <p className="text-zinc-500 leading-5 text-base mb-2">
-                  {incident.title}
-                </p>
+          {incidents.length > 0 &&
+            incidents.map((incident) => (
+              <li
+                className="p-6 rounded relative shadow flex justify-between"
+                key={incident.id}
+              >
+                <div>
+                  <strong className="block mb-1 text-zinc-600">CASO:</strong>
+                  <p className="text-zinc-500 leading-5 text-base mb-2">
+                    {incident.title}
+                  </p>
 
-                <strong className="block mb-1 text-zinc-600">DESCRIÇÂO:</strong>
-                <p className="text-zinc-500 leading-5 text-base mb-2">
-                  {incident.description}
-                </p>
+                  <strong className="block mb-1 text-zinc-600">
+                    DESCRIÇÂO:
+                  </strong>
+                  <p className="text-zinc-500 leading-5 text-base mb-2">
+                    {incident.description}
+                  </p>
 
-                <strong className="block mb-1 text-zinc-600">VALOR:</strong>
-                <p className="text-zinc-500 leading-5 text-base mb-2">
-                  {Intl.NumberFormat("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                  }).format(incident.value)}
-                </p>
-              </div>
-              <div>
-                <button
-                  className="border-0 hover:opacity-80"
-                  onClick={() => handleDeleteIncident(incident.id)}
-                  type="button"
-                >
-                  <FiTrash2 size={20} color="#a8a8b3" />
-                </button>
-              </div>
-            </li>
-          ))}
+                  <strong className="block mb-1 text-zinc-600">VALOR:</strong>
+                  <p className="text-zinc-500 leading-5 text-base mb-2">
+                    {Intl.NumberFormat("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    }).format(incident.value)}
+                  </p>
+                </div>
+                <div>
+                  <button
+                    className="border-0 hover:opacity-80"
+                    onClick={() => handleDeleteIncident(incident.id)}
+                    type="button"
+                  >
+                    <FiTrash2 size={20} color="#a8a8b3" />
+                  </button>
+                </div>
+              </li>
+            ))}
         </ul>
+        {incidents.length === 0 && (
+          <div className="h-96 w-full flex justify-center items-center">
+            <img src={heroImg} alt="Imagem de Super Herói" className="mr-10" />
+          </div>
+        )}
       </div>
     </div>
   );
