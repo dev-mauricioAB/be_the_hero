7721adbefdf267ba8ebf1ea4d94db0563e784d3e
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-06-28 07:20:29 -0300

    feat: lint adjusts

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes as ReactRoutes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes as ReactRoutes } from "react-router-dom";
 
-import Logon from './pages/Logon';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import NewIncident from './pages/NewIncident';
+import Logon from "./pages/Logon";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import NewIncident from "./pages/NewIncident";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <ReactRoutes>
-        <Route path='/' exact element={<Logon />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/incidents/new' element={<NewIncident />} />
+        <Route path="/" exact element={<Logon />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/incidents/new" element={<NewIncident />} />
       </ReactRoutes>
     </BrowserRouter>
   );
